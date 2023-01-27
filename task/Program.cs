fb9e629bb141c3bd66fbcf88a380f05bcb585328
PavlovVi <benthomasxl@gmail.com>
2023-01-27 22:05:12 +0300

    решение задачи, фильтрация, сбор нового массива

---
+++ task/Program.cs
@@ -0,0 +1,24 @@
+﻿//Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+//Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритмаю При решении не рекмендуется пользоваться коллекциями, лучше обойтись исключительно массивами. 
+
+string[] firstArray = new string[] { "apple", "vana", "-32", "dat", "Kazan" };
+     
+int count = 0;
+for (int i = 0; i < firstArray.Length; i++)
+{
+    if (firstArray[i].Length <= 3)
+    {
+        count++;
+    }
+}
+
+string[] secondArray = new string[count];
+int index = 0;
+for (int i = 0; i < firstArray.Length; i++)
+{
+    if (firstArray[i].Length <= 3)
+    {
+        secondArray[index] = firstArray[i];
+        index++;
+    }
+}
